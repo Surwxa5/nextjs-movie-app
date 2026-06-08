860e64b7c89eb72c667e664fabf5fde8bd8c9bdc
Surwxa5 <nepalisurakshya767@gmail.com>
2026-06-08 15:56:22 +0545

    search bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,8 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-  <div>
-    
-  </div>
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-
+'use client'
 import Image from "next/image";
 
 export default function Home() {
   return (
 <div>
-  
+  <h1>Movie App</h1>
+  <div style={{marginTop:'50px',textAlign:'center',}}>
+    <input type="text" placeholder="Enter a movie name"/>
+    <button style={{border:'1px solid black',width:'70px',marginLeft:'20px',borderRadius:'5px'}}>Search</button>
+  </div>
 </div>
   );
 }
