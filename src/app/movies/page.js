08c3f1f4b0abab1e50ed761c3a4b1ab68a3820df
Surwxa5 <nepalisurakshya767@gmail.com>
2026-06-13 12:46:18 +0545

    showing movies

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,21 +1,27 @@
-import { useSearchParams } from "next/navigation";
+
 // Imports useSearchParams from Next.js Imports useSearchParams from Next.js. This specific built-in hook lets you read the text after the ? in the current web address.
 export default async function movies({ searchParams }){
-  const res = await fetch('https://www.omdbapi.com/?s=batman&apikey=d455689b')
+    const useparams = await searchParams;
+    const query = useparams.search || 'batman';
+
+  const res = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=d455689b`)
   const data = await res.json()
 
-  const useparams = useSearchParams()
-        const Search = useparams.get('search')
+
   return(
     <div>
-      {/* <h1>
-        Movies
+      <h1>
+        Movies:{query}
       </h1>
-      {data.search.map((movie)=>(
-        <div key={movie.imdbID}>
-          <p>{movie.Title}</p>
-        </div>
-      ))} */}
+      <ul>
+      {data.Search?.map((movie)=>(
+        
+        <li key={movie.imdbID}>
+        
+          {movie.Title} ({movie.Year})
+        </li>
+      ))} 
+      </ul>
     </div>
   )
 }
