d09994e860464658ae5a9ac0c9851d7ce6c1f24d
Surwxa5 <nepalisurakshya767@gmail.com>
2026-06-14 11:42:35 +0545

    complete image fetching

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -16,8 +16,10 @@ export default async function movies({ searchParams }){
 
       <div style={{display:'flex', flexWrap:'wrap',marginTop:'30px', justifyContent:'center',gap:'20px'}}>
       {data.Search?.map((movie)=>(
-        <div key={movie.imdbID}>
+        <div key={movie.imdbID} style={{ width: '150px', textAlign: 'center' }}>
           {/* for image poster */}
+          {/* using ternery operater */}
+          {movie.Poster !== 'N/A'?(
           <img
              src={movie.Poster}
              width={150}
@@ -25,7 +27,9 @@ export default async function movies({ searchParams }){
              style={{ borderRadius: '8px', objectFit: 'cover' }}
              height={220}
                />
-
+            ):(
+            <div style={{width:'150px', borderRadius:'8px', display:'flex',alignItems:'center',justifyContent:'center',height:'220px',background:'#ccc'}}
+            >No Image </div>)}
          <p style={{ marginTop: '8px', fontWeight: 'bold', fontSize: '14px' }}>{movie.Title}</p>
          <p style={{ fontSize: '12px', color: 'gray' }}>{movie.Year}</p>
         </div>
